96e79bb2440b4e68cef2a8448596e2eb37ad8036
lbosch <lbosch@icra.cat>
2017-03-02 03:46:17 +0100

    proves server.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express=require('express');
 var app=express();
 app.listen(3000,function(){console.log('app escoltant al port 3000')});
 
-app.get('/',          function(req,res){
-	console.log(app.mountpath);
-	res.sendFile(__dirname+"/index.html")});
-app.get('/hola',      function(req,res){res.send("hola que tal <a href=/>Inici</a>")});
+//root
+app.get('/',function(req,res){
+	res.sendFile(__dirname+"/index.html")
+});
 
+//GET hola
+app.get('/hola',function(req,res){
+	res.send("hola que tal <a href=/>Inici</a>")
+});
--- tests/connection.js
+++ tests/connection.js
@@ -2,7 +2,7 @@ const mdb=require('mongodb').MongoClient;
 
 //connect to mongodb
 mdb.connect("mongodb://localhost/Corrupcio",function(err,db){
-	console.log(err);
+	if(err!==null){console.log('error');return}
 	console.log("Connectat a la base de dades");
 	db.close();
 });
