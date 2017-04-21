0224c62dadfb427e857dd3f69982a0488730c183
lbosch <lbosch@icra.cat>
2017-04-21 22:42:33 +0200

    top5 canvi

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
 <hr><!--top 5 casos-->
 <div>
 	<table id=top5>
-		<tr><th colspan=2>Top 5 casos
+		<tr><th colspan=3>Top 5 casos
 		<?php
 			$sql="SELECT * FROM casos ORDER BY espoli DESC LIMIT 5";
 			$res=$mysql->query($sql) or die(mysqli_error($mysql));
